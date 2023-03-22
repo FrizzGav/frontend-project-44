ff4e17ad9fed5e1b43f8a3a9b5cc5c348ed317dd
FrizzGav <slava.levchenko.03@mail.ru>
2023-03-22 15:28:44 +0300

    ff

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
+console.log("Welcome to the Brain Games!");
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
-console.log("Welcome to the Brain Games!");
+const name = readlineSync.question('May I have your name? ')
+console.log("Hello," , name +"!")
