a5c327fb287091dbc65e82a867dba4c65865802d
FrizzGav <slava.levchenko.03@mail.ru>
2023-03-29 13:02:28 +0300

    rif

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,4 @@
 import readlineSync from 'readline-sync';
 
-export default function nameAsk() {
-  const userName = readlineSync.question('May I have your name? ');
-  return userName;
-}
+const nameSecond = readlineSync.question('May I have your name? ')
+console.log("Hello," , nameSecond +"!");
